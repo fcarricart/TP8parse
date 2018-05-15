32073d1d7ef8d74769d8db15fabadf5016602ca7
Unknown <fcarricart@itba.edu.ar>
2018-05-15 12:09:24 -0300

    listo

--- Parsecmd.cpp
+++ Parsecmd.cpp
@@ -1,12 +1,15 @@
 
 #include "parsecmd.h"
+#include <string.h>
 #include "parsecallback.h"
 
 int parseCmdLine(int argc, char *argv[], pCallback p, void *userData) {
 	int result = 0, error = false;
 
-	for (int i = 1; i<argc;) {
-		if (argv[i][0] == '-') {
+	for (int i = 1; i<argc;) 
+	{
+		if (argv[i][0] == '-') 
+		{
 			if (argv[i][1] == 0) //- unico valor
 			{
 				error = ERROR2;
@@ -20,15 +23,30 @@ int parseCmdLine(int argc, char *argv[], pCallback p, void *userData) {
 			
 			else
 			{
-				result = p(&(argv[i][1]), argv[i + 1], userData);
-				if (result == 1) {
-					i += 2;
-				}
-				else {
-					error = ERROR3;
+				if (argv[i + 1][0] == '-')
+				{
+					error = ERROR1;
 					i = argc;
 				}
+				else
+				{
+					result = p(&(argv[i][1]), argv[i + 1], userData);
+					if (result == 1) 
+					{
+						i += 2;
+					}
+					else 
+					{
+						error = ERROR3;
+						i = argc;
+					}
+				}
 			}
+		}
+		else
+		{
+			error = ERROR2;
+			i = argc;
 		}
 	}
 	if (error == ERROR1)
